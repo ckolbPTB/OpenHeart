47fe77a41aa3a5b79a6b354409d12b113c5fdaa2
johannesmayer <johannes.mayer1991@gmail.com>
2022-06-29 14:54:14 +0000

    Deleted comment

--- recon/gt_grappa.sh
+++ recon/gt_grappa.sh
@@ -5,7 +5,6 @@ echo "Running GRAPPA reconstruction using Gadgetron."
 fname_in=$1
 fdir_out=$2
 fname_config="./gtpipelines/Generic_Cartesian_Grappa.xml"
-# fname_config="./gtpipelines/dicom.xml"
 
 echo "Input file is ${fname_in} and output directory is ${fdir_out}"
 echo "Stream configuration is ${fname_config}}"
@@ -15,7 +14,7 @@ dir_of_recon=$(pwd)
 gadgetron_ismrmrd_client --filename=$fname_in --outfile=" " --config-local=$fname_config
 
 # deleting junk files
-find -type f -name "*_attrib.xml" -delete 
+find -type f -name "*_attrib.xml" -delete
 
-#  
+#
 python3 gtpipelines/postprocess_dcm.py -i=$dir_of_recon -o=${fdir_out}
