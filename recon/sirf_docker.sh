0d02ae2d75f6fc1b9dba38242063664b27da12ab
johannesmayer <johannes.mayer1991@gmail.com>
2022-07-27 12:46:34 +0200

    Added command line argument to be used as filepath to sirf docker

--- recon/sirf_docker.sh
+++ recon/sirf_docker.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
-fpath_data="/media/sf_CCPPETMR/XNAT/xnat_example_data/Subject1/"
-container_id=$(docker run -d -it -v $fpath_data:/tmp ckolbptb/sirf_qc)
-# container_id=$(docker run -d -it -v /media/sf_CCPPETMR/XNAT/Temp/:/tmp ckolbptb/sirf_qc)
+fpath_data=$1
+#image_name="johannesmayer/sirf_qc"
+image_name="ckolbptb/sirf_qc"
+container_id=$(docker run -d -it -v $fpath_data:/tmp ${image_name})
 
 
 fname_reconscript="mr_cine_recon.py"
 fname_dcm_dummy="dummyfile.dcm"
 
 docker cp ./$fname_reconscript $container_id:/tmp
-docker cp ./$fname_dcm_dummy $container_id:/tmp
 
 docker exec -it $container_id /bin/bash -c "source ~/.bashrc && gadgetron & source ~/.bashrc && python3 /tmp/${fname_reconscript} /tmp /tmp"
 
