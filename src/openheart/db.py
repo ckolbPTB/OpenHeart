f0e6d0d14ede89cf0d8abe267d56a2bfa8d2b4ff
johannesmayer <johannes.mayer1991@gmail.com>
2022-08-23 10:58:44 +0000

    Initialised database object

--- src/openheart/db.py
+++ src/openheart/db.py
@@ -14,7 +14,7 @@ def get_db():
     return g.db
 
 
-def close_db():
+def close_db(e=None):
     db = g.pop('db', None)
 
     if db is not None:
@@ -33,5 +33,5 @@ def init_db_command():
     click.echo("Initialized the database.")
 
 def init_app(app):
-    app.teardown_context(close_db)
+    app.teardown_appcontext(close_db)
     app.cli.add_command(init_db_command)
