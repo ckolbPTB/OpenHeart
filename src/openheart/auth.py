9e723caebaf89548ad36233dfd56fa5d1d5c1663
johannesmayer <johannes.mayer1991@gmail.com>
2022-08-23 14:57:51 +0000

    Added authentification blueprint

---
+++ src/openheart/auth.py
@@ -0,0 +1,54 @@
+from sqlite3 import IntegrityError
+from flask import (
+    Blueprint, flash, g, redirect, render_template, request, session, url_for
+)
+from werkzeug.security import check_password_hash, generate_password_hash
+
+
+from openheart.db import get_db
+
+bp = Blueprint('auth', __name__, url_prefix='/auth')
+
+@bp.route("/register", methods=['GET','POST'])
+def register():
+
+    if request.method == 'POST':
+
+        email = request.form['email']
+        password = request.form['password']
+
+        print(f"We have {email}")
+        print(f"We have {password}")
+
+        db = get_db()
+
+        error = None
+        if not email:
+            error = "Email is required."
+        if not password:
+            error = "Password is required."
+
+        if error is None:
+            try:
+                db.execute(
+                    "INSERT INTO user (email, password) VALUES (?, ?)",
+                    (email, generate_password_hash(password)),
+                    )
+                db.commit()
+            except db.IntegrityError:
+                error = f"Email {email} is already registered."
+            else:
+                return redirect(url_for("auth.login"))
+
+        flash(error)
+
+    return render_template('auth/register.html')
+
+
+@bp.route("/login", methods=['GET', 'POST'])
+def login():
+
+    # if request.method == "POST":
+    #     pass
+
+    return render_template("auth/login.html")
