230cfc908aa0342ffeb2ecbd5aa78620a17f6120
johannesmayer <johannes.mayer1991@gmail.com>
2022-08-24 13:24:26 +0000

    Ensuring valid database creation

--- src/openheart/__init__.py
+++ src/openheart/__init__.py
@@ -1,15 +1,18 @@
 import os
 from flask import Flask
-
 from flask_sqlalchemy import SQLAlchemy
+
 db = SQLAlchemy()
+
+from openheart.user import User
+
 
 def create_app(test_config=None):
     # create and configure the app
     app = Flask(__name__, instance_relative_config=True)
     app.config.from_mapping(
         SECRET_KEY='dev',
-        SQLALCHEMY_DATABASE_URI = 'sqlite:///open_heart.db'
+        SQLALCHEMY_DATABASE_URI = 'sqlite:////db/open_heart.db'
     )
 
     if test_config is None:
