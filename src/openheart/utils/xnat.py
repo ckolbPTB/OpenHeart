5936896d636431919a1058c281c53ea1b922031e
Christoph Kolbitsch <ptbgenreg@gmail.com>
2023-04-02 20:35:51 +0200

    scan type added to h5 not scan id

--- src/openheart/utils/xnat.py
+++ src/openheart/utils/xnat.py
@@ -78,16 +78,22 @@ def set_xnat_ids_in_files(list_files: list) -> list:
     for idx, f in enumerate(list_files):
         experiment_id = utils.create_md5_from_string(f.subject_unique)
         experiment_id = f"Exp-{experiment_id[:digits_experiment_id]}"
-        scan_id = f'Scan_{uuid4()}-{utils.scan_types[f.scan_type]}'
+        scan_id = f'Scan_{uuid4()}'
 
         f.xnat_subject_id = f.subject_unique
         f.xnat_experiment_id = experiment_id
         f.xnat_scan_id = scan_id
 
+        # Add scan type ID to file name
+        file_name_h5_with_scan_type = f.name_unique.replace('.h5', f'-{utils.scan_types[f.scan_type]}.h5')
+        Path(f.name_unique).rename(file_name_h5_with_scan_type)
+        f.name_unique = file_name_h5_with_scan_type
+
         current_app.logger.info(f'Xnat parameters for {f.name}:')
         current_app.logger.info(f'   subject_id: {f.xnat_subject_id}')
         current_app.logger.info(f'   experiment_id: {f.xnat_experiment_id}')
         current_app.logger.info(f'   scan_id: {f.xnat_scan_id}')
+        current_app.logger.info(f'   name_unique: {f.name_unique}')
 
     return list_files
 
